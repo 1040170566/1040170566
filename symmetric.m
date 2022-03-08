7e8cabb2515463a82a9de6e0db59b172d600ac43
把朕的群论拿过来 <63913707+1040170566@users.noreply.github.com>
2022-03-08 23:35:26 +0800

    列出一个n维数组的n！种排列方式

---
+++ symmetric.m
@@ -0,0 +1,20 @@
+function [p,c]=symmetric(array)
+%此函数用于列出置换群的所有可能情况
+%p为实际置换结果，c为对应循环表示
+
+p=[];
+L=length(array);
+if L==1
+    p=array;
+else
+    for i=1:L
+        q=repmat(array(i),factorial(L-1),1);
+        array_new=array;
+        array_new(i)=[];
+        p_sym=cat(2,q,symmetric(array_new));
+        p=cat(1,p,p_sym);
+    end
+end
+
+% if nargout==2
+%     
